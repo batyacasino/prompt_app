ba10a1466cec556477acea247a279539a2d24a9c
batyacasino <51843016+batyacasino@users.noreply.github.com>
2020-06-10 18:09:03 +0300

    Update 0002_client_date_of_birth.py

--- kosapp/migrations/0002_client_date_of_birth.py
+++ kosapp/migrations/0002_client_date_of_birth.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='client',
             name='date_of_birth',
-            field=models.DateField(default='2000-21-12'),
+            field=models.DateField(default='2000-12-21'),
             preserve_default=False,
         ),
     ]
