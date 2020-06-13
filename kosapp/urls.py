baa6a996e740a1b470ac931e487b8a7ca2f10bd5
batyacasino <51843016+batyacasino@users.noreply.github.com>
2020-06-13 20:55:15 +0300

    Update urls.py

--- kosapp/urls.py
+++ kosapp/urls.py
@@ -8,10 +8,4 @@ urlpatterns = [
 	path('', index, name='index'),
 	path('add_client/', AddClient.as_view(), name='add_client_url'),
 	path('detail_client/<int:pk>/', detail_client, name='detail_client_url'),
-	path('detail_client/<int:pk>/upload_docs/', UploadDocs.as_view(), name='upload_docs_url'),
-	path('upload/', upload, name='upload_url'),
 ]
-
-
-if settings.DEBUG:
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
