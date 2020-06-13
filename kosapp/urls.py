4e04cf7318cc32540e520b74a32ef9c3a876b78b
batyacasino <51843016+batyacasino@users.noreply.github.com>
2020-06-13 19:08:52 +0300

    Update urls.py

--- kosapp/urls.py
+++ kosapp/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
 	path('', index, name='index'),
 	path('add_client/', AddClient.as_view(), name='add_client_url'),
 	path('detail_client/<int:pk>/', detail_client, name='detail_client_url'),
-	path('detail_client/<int:pk>/update/', ClientUpdate.as_view(), name='client_update_url'),
-	path('detail_client/<int:pk>/delete/', ClientDelete.as_view(), name='client_delete_url'),
-	path('alldelete/', alldelete, name='alldelete'),
+	path('detail_client/<int:pk>/upload_docs/', UploadDocs.as_view(), name='upload_docs_url'),
+	path('upload/', upload, name='upload_url'),
 ]
+
+
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
